36ffacb2f7a8f637fb0b546ebc0e08e4714d2843
Magno <magnodiasp@gmail.com>
2024-11-12 22:09:30 -0300

    chore: aula 3 finalizada

---
+++ app/_actions/upsert-transaction/index.ts
@@ -0,0 +1,49 @@
+"use server";
+
+import { db } from "@/app/_lib/prisma";
+import { auth } from "@clerk/nextjs/server";
+import {
+  TransactionCategory,
+  TransactionPaymentMethod,
+  TransactionType,
+} from "@prisma/client";
+import { upsertTransactionSchema } from "./schema";
+import { revalidatePath } from "next/cache";
+
+interface UpsertTransactionParams {
+  id?: string;
+  name: string;
+  amount: number;
+  type: TransactionType;
+  category: TransactionCategory;
+  paymentMethod: TransactionPaymentMethod;
+  date: Date;
+}
+
+export const upsertTransaction = async (params: UpsertTransactionParams) => {
+  upsertTransactionSchema.parse(params);
+  const { userId } = await auth();
+  if (!userId) {
+    console.log("Sem autorização");
+    throw new Error("Unauthorized");
+  }
+
+  if (params.id) {
+    // Atualizar se o id estiver presente
+    await db.transaction.upsert({
+      where: {
+        id: params.id,
+      },
+      update: { ...params, userId },
+      create: { ...params, userId },
+    });
+  } else {
+    // Criar nova transação se o id estiver ausente
+    await db.transaction.create({
+      data: { ...params, userId },
+    });
+  }
+
+  console.log("Autorizado");
+  revalidatePath("/transactions");
+};
--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,6 +1,6 @@
-import { LogInIcon } from "lucide-react";
 import Image from "next/image";
 import { Button } from "../_components/ui/button";
+import { LogInIcon } from "lucide-react";
 import { SignInButton } from "@clerk/nextjs";
 import { auth } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
@@ -13,7 +13,6 @@ const LoginPage = async () => {
   return (
     <div className="grid h-full grid-cols-2">
       {/* ESQUERDA */}
-
       <div className="mx-auto flex h-full max-w-[550px] flex-col justify-center p-8">
         <Image
           src="/logo.svg"
@@ -23,7 +22,7 @@ const LoginPage = async () => {
           className="mb-8"
         />
         <h1 className="mb-3 text-4xl font-bold">Bem-vindo</h1>
-        <p className="text-muted-foreground mb-8">
+        <p className="mb-8 text-muted-foreground">
           A Finance AI é uma plataforma de gestão financeira que utiliza IA para
           monitorar suas movimentações, e oferecer insights personalizados,
           facilitando o controle do seu orçamento.
@@ -35,12 +34,11 @@ const LoginPage = async () => {
           </Button>
         </SignInButton>
       </div>
-
       {/* DIREITA */}
       <div className="relative h-full w-full">
         <Image
           src="/login.png"
-          alt="Faço o Login"
+          alt="Faça login"
           fill
           className="object-cover"
         />
